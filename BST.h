7cc86fb5229177987774b957e072d63f036808f7
lawrencekhlim <lawrencekhlim@gmail.com>
2018-02-06 23:44:13 -0800

    Fixed rangeSearch for BST

--- BST.h
+++ BST.h
@@ -9,7 +9,7 @@ class BST
 		node* head;
         void insert(string str, node* head);
         int search(string str, node* head);
-        void rangeSearches (string a, string b, node* pointer, vector <string> * list);
+        void rangeSearches (string a, string b, node* pointer, vector <string> &list);
 	public:
 		BST();
         ~BST();
@@ -19,6 +19,6 @@ class BST
 		int search(string str);
 		void readInTree(string a[],int length);
 		node* getHead();
-        vector <string>* rangeSearch (string a, string b);
+        vector <string>rangeSearch (string a, string b);
 };
 #endif
